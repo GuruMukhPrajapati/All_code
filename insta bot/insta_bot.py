638e8e67e7fbeef12c11b553ba34c4aab78197e1
Gourav prajapat <117162170+GuruMukhPrajapati@users.noreply.github.com>
2023-05-24 17:29:04 +0530

    Update insta_bot.py

--- insta bot/insta_bot.py
+++ insta bot/insta_bot.py
@@ -5,3 +5,6 @@ bot.login(username="YOUR_LOGIN", password="YOUR_PASSWORD")
 user_id = bot.get_user_id_from_username("lego")
 user_info = bot.get_user_info(user_id)
 print(user_info['biography'])
+
+
+https://www.instagram.com/pulkitmaheshwari23/
